5e18462f685a0419d414f826cb65b35c8bcfac8e
Nate <robertanetanyatan@gmail.com>
2024-11-11 17:50:20 +0800

    first commit

---
+++ maushold.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void mouseBattle(int rHP, int mATK) 
+{
+    int hits = 0;
+    printf("Maushold used Population Bomb!\n");
+
+    for (int i = mATK; i > 0 && hits < 10; i--) 
+    {
+        if (mATK % i == 0) 
+        { // Check if 'i' is a factor
+            rHP -= i;
+            hits++;
+
+            if (rHP < 0) rHP = 0;
+            
+            printf("It hit %d time(s)! The opposing Rattata has %d HP left.\n", hits, rHP);
+
+
+            if (rHP <= 0) 
+            {
+                printf("The opposing Rattata has fainted!\n");
+                return;
+            }
+        }
+    }
+
+    // If Rattata still has HP left after the loop
+    if (rHP > 0) 
+        printf("Uh oh, the Rattata still lives with %d HP!\n", rHP);
+}
